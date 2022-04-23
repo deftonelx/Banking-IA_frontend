ec4cc6a9a1bd26d030f0143766bcc9b9b7362e1e
APEREZB26 <aperez26@autonoma.edu.pe>
2022-04-23 11:00:11 -0500

    [ADD] NAVBAR

--- src/components/ui/NavBar.js
+++ src/components/ui/NavBar.js
@@ -1,7 +1,42 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export const NavBar = () => {
+  const handleBar = () => {
+    const navburger = document.querySelector('.navbar__phone');
+    navburger.classList.toggle('navbar__phone--active');
+  };
+
   return (
-    <div>NavBar</div>
-  )
-}
+    <nav className="navbar">
+      <div className="navbar__logo">
+        <img src="../assets/components/react.png" alt="logo" />
+      </div>
+      <div className="navbar__links">
+        <NavLink to="#" className="navbar__link navbar__link-active">
+          Inicio
+        </NavLink>
+        <NavLink to="#" className="navbar__link">
+          Operaciones
+        </NavLink>
+        <NavLink to="#" className="navbar__link">
+          Ajustes
+        </NavLink>
+        <button className="navbar__button" onClick={handleBar}>
+          <i className="fa-solid fa-bars"></i>
+        </button>
+      </div>
+      <div className="navbar__phone">
+        <NavLink to="#" className="navbar__bur navbar__link-active">
+          Inicio
+        </NavLink>
+        <NavLink to="#" className="navbar__bur">
+          Operaciones
+        </NavLink>
+        <NavLink to="#" className="navbar__bur">
+          Ajustes
+        </NavLink>
+      </div>
+    </nav>
+  );
+};
