88b152845a690396851f3f23ff9c6d1c3835e5f0
Yactayo <syactayocrisostomo@gmail.com>
2022-04-23 11:21:11 -0500

    [UPDATE] style redesign

--- src/components/banking/MainScreen.js
+++ src/components/banking/MainScreen.js
@@ -1,7 +1,5 @@
-export const MainScreen = () =>{
-    return(
-        <div>
-            <h1>Esta es la MainScreen</h1>
-        </div>
-    )
-}
+import { NavBar } from '../ui/NavBar';
+
+export const MainScreen = () => {
+  return <NavBar />;
+};
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { LoginScreen } from '../components/auth/LoginScreen';
 import { RegisterScreen } from '../components/auth/RegisterScreen';
-import {MainScreen} from '../components/banking/MainScreen'
+import { MainScreen } from '../components/banking/MainScreen';
 
 export const AppRouter = () => {
   return (
@@ -10,7 +10,7 @@ export const AppRouter = () => {
       <Routes>
         <Route path="/auth/login" element={<LoginScreen />} />
         <Route path="/auth/register" element={<RegisterScreen />} />
-        <Route path="/auth/main" element={<MainScreen/>}/>
+        <Route path="/main" element={<MainScreen />} />
 
         <Route path="/*" element={<Navigate to="/auth/login" />} />
       </Routes>
